1a5311c127df9163f3b7a3b2d50635e8dc215046
mpruneda87 <mpruneda87@gmail.com>
2017-10-20 23:31:15 -0500

    Implement replay system

--- TwinStick/Assets/ReplaySystem.cs
+++ TwinStick/Assets/ReplaySystem.cs
@@ -5,53 +5,96 @@ using UnityEngine;
 public class ReplaySystem : MonoBehaviour {
 
     private const int bufferFrames = 100;
-    private MyKeyFrame[] keyFrames = new MyKeyFrame[bufferFrames];
+    private GameKeyFrame[] keyFrames = new GameKeyFrame[bufferFrames];
 
     private Rigidbody rigidBody;
 
-	// Use this for initialization
-	void Start () {
+    private GameManager gameManager;
+
+    private bool recordToggle;  // This tracks when user switches between Record and Playback
+    private int startKeyFrame;  // Frame recording begin at
+    private int totalKeyFrame;  // How many frames were recorded
+    private int currentKeyFrame; // Current position in Playback
+
+
+
+    // Use this for initialization
+    void Start () {
         rigidBody = GetComponent<Rigidbody>();
-	}
+        gameManager = FindObjectOfType<GameManager>();
+        recordToggle = !gameManager.recording;
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        Record();
+        if (gameManager.recording == true)
+        { Record(); }
+        else
+        { Playback(); }
+    }
+    private void Playback()
+    {
+        if (recordToggle != gameManager.recording)  // Started playback
+        {
+            recordToggle = gameManager.recording;
+            rigidBody.isKinematic = true;
+
+            int endKeyFrame = Time.frameCount;
+            if ((endKeyFrame - startKeyFrame) >= bufferFrames) // Recorded past buffer limit.  Playback entire buffer
+            {
+                startKeyFrame = endKeyFrame + 1;
+                totalKeyFrame = bufferFrames;
+            }
+            else  // Only playback recorded portion
+            {
+                totalKeyFrame = endKeyFrame - startKeyFrame;
+            }
+
+            currentKeyFrame = 0;
+        }
+
+        int keyFrame = (startKeyFrame + currentKeyFrame) % bufferFrames;
+        Debug.Log("keyFrames[" + keyFrame + "].keyTime = " + keyFrames[keyFrame].frameTime);
+
+        transform.position = keyFrames[keyFrame].position;
+        transform.rotation = keyFrames[keyFrame].rotation;
+
+        currentKeyFrame++;
+        if (currentKeyFrame >= totalKeyFrame) // Replay from beginning if exceeded recorded portion
+        {
+            currentKeyFrame = 0;
+        }
     }
 
-    void PlayBack() {
-        rigidBody.isKinematic = true;
-        int frame = Time.frameCount % bufferFrames;
-        print("REading frame " + frame);
-        transform.position = keyFrames[frame].position;
-        transform.rotation = keyFrames[frame].rotation;
-
-    }
 
     private void Record()
     {
-        rigidBody.isKinematic = false;
-        int frame = Time.frameCount % bufferFrames;
-        float time = Time.time;
+        if (recordToggle != gameManager.recording)  // Started recording
+        {
+            recordToggle = gameManager.recording;
+            rigidBody.isKinematic = false;
+            startKeyFrame = Time.frameCount;
+        }
 
-        print("Writing frame " + frame);
-
-        keyFrames[frame] = new MyKeyFrame(time, transform.position, transform.rotation);
+        int keyFrame = Time.frameCount % bufferFrames;
+        keyFrames[keyFrame] = new GameKeyFrame(Time.time, transform.position, transform.rotation);
     }
+
 }
+
 
 /// <summary>
 /// A structure for storing time, rotation, and position.
 /// </summary>
-public struct MyKeyFrame {
+public struct GameKeyFrame {
 
     public float frameTime;
     public Vector3 position;
     public Quaternion rotation;
 
     //constructor
-    public MyKeyFrame(float aTime, Vector3 aPosition, Quaternion aRotation)
+    public GameKeyFrame(float aTime, Vector3 aPosition, Quaternion aRotation)
     {
         frameTime = aTime;
         position = aPosition;
